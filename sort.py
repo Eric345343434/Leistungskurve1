60bf51a943bab71f5c3eb2cc64fbea689893a18a
Eric345343434 <je3301@mci4me.at>
2024-04-11 14:49:34 +0200

    csv import

--- sort.py
+++ sort.py
@@ -1,1 +1,6 @@
-print("Hello")
+import csv
+
+with open("activity.csv") as csvdatei:
+    csv_reader_object = csv.reader(csvdatei, delimiter=";")
+    for row in csv_reader_object:
+        print(row)
