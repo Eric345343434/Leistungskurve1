d1b4c54c95595a6165e58e7d0135e862cd51a5c4
Eric345343434 <je3301@mci4me.at>
2024-04-11 15:08:45 +0200

    change

--- sort.py
+++ sort.py
@@ -1,6 +1,19 @@
-import csv
+def bubble_sort(arr):
+ 
+    n = len(arr)
+    swapped = False  # Optimierung: Wenn keine Vertauschungen erfolgen, ist das Array bereits sortiert
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Elemente vertauschen, wenn sie in der falschen Reihenfolge sind
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            # Wenn keine Vertauschungen vorgenommen wurden, die Hauptschleife verlassen
+            break
 
-with open("activity.csv") as csvdatei:
-    csv_reader_object = csv.reader(csvdatei, delimiter=";")
-    for row in csv_reader_object:
-        print(row)
+
+
+    
+
+    
